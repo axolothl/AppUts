373c394d335b81a45121cc6dde7434c5479a33e0
gregoriusandito <gregorius.andito@gmail.com>
2016-10-28 13:45:46 +0700

    Add Dummy GMaps and Splash Screen

--- app/src/main/java/com/example/setditjenp2mkt/apputs/KulinerActivity.java
+++ app/src/main/java/com/example/setditjenp2mkt/apputs/KulinerActivity.java
@@ -3,12 +3,32 @@ package com.example.setditjenp2mkt.apputs;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class KulinerActivity extends AppCompatActivity {
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class KulinerActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_kuliner);
         setTitle("Info Kuliner");
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        CameraPosition googlePlex = CameraPosition.builder()
+                .target(new LatLng(5.547754,95.315221))
+                .zoom(16)
+                .bearing(0)
+                .tilt(45)
+                .build();
+
+        googleMap.addMarker(new MarkerOptions()
+                .position(new LatLng(5.547754,95.315221))
+                .title("Musium Tsunami Aceh"));
     }
 }
