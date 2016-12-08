d41e8e46db2d2dbd427a3991e254a4a7907bf9c5
gregoriusandito <gregorius.andito@gmail.com>
2016-12-08 13:50:42 +0700

    Implement JSON and mysql DB from local server

---
+++ app/src/main/java/com/example/setditjenp2mkt/apputs/utils/Global.java
@@ -0,0 +1,17 @@
+package com.example.setditjenp2mkt.apputs.utils;
+
+/**
+ * Created by Gregorius Andito on 4/1/2016.
+ */
+public class Global {
+
+    public static final String BASE_URL="http://192.168.0.105/kotamu/";
+    public static final String BASE_IMG=BASE_URL+"img_kota/";
+    public static final String GET_KOTA=BASE_URL+"getKota.php";
+    public static final String DETAIL_BERITA=BASE_URL+"detailKota.php";
+
+    //global variable
+    public static final String ID = "id";
+    public static final String KOTA = "kota";
+    public static final String GAMBAR = "gambar";
+}
