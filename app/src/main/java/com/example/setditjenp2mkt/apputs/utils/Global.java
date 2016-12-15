316274c262a25db3933cd8a9e19f32fc318c102c
gregoriusandito <gregorius.andito@gmail.com>
2016-12-15 14:29:50 +0700

    Login Activity On Progress

--- app/src/main/java/com/example/setditjenp2mkt/apputs/utils/Global.java
+++ app/src/main/java/com/example/setditjenp2mkt/apputs/utils/Global.java
@@ -5,10 +5,19 @@ package com.example.setditjenp2mkt.apputs.utils;
  */
 public class Global {
 
-    public static final String BASE_URL="http://mobcompfinal.pe.hu/";
-    public static final String BASE_IMG=BASE_URL+"img_kota/";
-    public static final String GET_KOTA=BASE_URL+"getKota.php";
-    public static final String DETAIL_KOTA=BASE_URL+"detailKota.php";
+//    public static final String BASE_URL="http://mobcompfinal.pe.hu/";
+//    public static final String BASE_IMG=BASE_URL+"img_kota/";
+//    public static final String GET_KOTA=BASE_URL+"getKota.php";
+//    public static final String DETAIL_KOTA=BASE_URL+"detailKota.php";
+
+    public static final String BASE_URL="http://192.168.0.107/pantura/Welcome/";
+    public static final String BASE_IMG="http://192.168.0.107/kotamu/img_kota/";
+    public static final String GET_KOTA=BASE_URL+"get_kota";
+    public static final String DETAIL_KOTA=BASE_URL+"detail_kota";
+
+
+    public static final String URL_LOGIN = "http://192.168.0.107/pantura/android_login_api/login.php";
+    public static final String URL_REGISTER = "http://192.168.0.107/pantura/android_login_api/register.php";
 
     //global variable
     public static final String ID = "id";
