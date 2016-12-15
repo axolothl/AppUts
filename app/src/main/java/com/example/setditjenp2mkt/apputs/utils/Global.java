859bc3c46b05cba37db1b8da90344dec88995cb0
gregoriusandito <gregorius.andito@gmail.com>
2016-12-15 14:40:17 +0700

    On Progress Creating Login Activity

--- app/src/main/java/com/example/setditjenp2mkt/apputs/utils/Global.java
+++ app/src/main/java/com/example/setditjenp2mkt/apputs/utils/Global.java
@@ -11,13 +11,13 @@ public class Global {
 //    public static final String DETAIL_KOTA=BASE_URL+"detailKota.php";
 
     public static final String BASE_URL="http://192.168.0.107/pantura/Welcome/";
-    public static final String BASE_IMG="http://192.168.0.107/kotamu/img_kota/";
+    public static final String BASE_IMG="http://192.168.0.107/pantura/img_kota/";
     public static final String GET_KOTA=BASE_URL+"get_kota";
     public static final String DETAIL_KOTA=BASE_URL+"detail_kota";
 
 
-    public static final String URL_LOGIN = "http://192.168.0.107/pantura/android_login_api/login.php";
-    public static final String URL_REGISTER = "http://192.168.0.107/pantura/android_login_api/register.php";
+    public static final String URL_LOGIN = "http://192.168.0.107/pantura/Welcome/login_endpoint";
+    public static final String URL_REGISTER = "http://192.168.0.107/pantura/Welcome/registration_endpoint";
 
     //global variable
     public static final String ID = "id";
